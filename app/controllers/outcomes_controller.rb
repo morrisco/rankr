b4f886954182bcda9066563a4d90aece169daa48
Max Morris-Cohen <maxler@gmail.com>
2013-07-13 14:18:19 -0500

    added links to navigate pages

--- app/controllers/outcomes_controller.rb
+++ app/controllers/outcomes_controller.rb
@@ -1,12 +1,16 @@
 class OutcomesController < ApplicationController
 	def new
+		@outcome = Outcome.new
 	end
 
 	def create
 		@outcome = Outcome.new(outcome_params)
 
-		@outcome.save
-		redirect_to @outcome
+		if @outcome.save
+			redirect_to @outcome
+		else
+			render 'new'
+		end
 	end
 
 	def show
@@ -15,6 +19,20 @@ class OutcomesController < ApplicationController
 
 	def index
 		@outcomes = Outcome.all
+	end
+
+	def edit
+		@outcome = Outcome.find(params[:id])
+	end
+
+	def update
+		@outcome = Outcome.find(parmas[:id])
+		
+		if @outcome.update(outcome_params)
+			redirect_to @outcome
+		else
+			render 'edit'
+		end
 	end
 
 	private
--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -1,2 +1,4 @@
 class Outcome < ActiveRecord::Base
+	validates :winner, presence: true, length: { minimum: 5 }
+	validates :loser, presence: true, length: { minimum: 5 }
 end
